e76efe72253c9ba04b3234f673f7b3f50368432e
cakefeelsgood <kimminji15441544@gmail.com>
2026-05-11 10:20:18 +0900

    feat: 신청 좌석 락 만료 복구 스케줄러 구현 (#239)

--- src/main/java/com/moment/momentbackend/application/repository/ApplicationRepository.java
+++ src/main/java/com/moment/momentbackend/application/repository/ApplicationRepository.java
@@ -3,8 +3,12 @@ package com.moment.momentbackend.application.repository;
 import com.moment.momentbackend.application.entity.Application;
 import com.moment.momentbackend.application.type.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
@@ -17,4 +21,15 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
     );
 
     Optional<Application> findByIdAndUserId(Long id, Long userId);
+
+    @Query("""
+            SELECT a FROM Application a
+            WHERE a.applicationStatus IN :statuses
+            AND a.seatLockedUntil IS NOT NULL
+            AND a.seatLockedUntil < :now
+            """)
+    List<Application> findExpiredApplications(
+            @Param("statuses") List<ApplicationStatus> statuses,
+            @Param("now") LocalDateTime now
+    );
 }
