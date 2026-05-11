c25e6355856f3e75dbb232ef255463570b8a289b
armddi <jungarum0607@gmail.com>
2026-05-11 16:08:17 +0900

    feat: 추천 설문 진행률 조회 API 구현 #199

--- src/main/java/com/moment/momentbackend/child/repository/ChildConcernRepository.java
+++ src/main/java/com/moment/momentbackend/child/repository/ChildConcernRepository.java
@@ -3,6 +3,9 @@ package com.moment.momentbackend.child.repository;
 import com.moment.momentbackend.child.entity.ChildConcern;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChildConcernRepository extends JpaRepository<ChildConcern, Long> {
     void deleteAllByChildProfileId(Long childProfileId);
+    List<ChildConcern> findByChildProfileId(Long childProfileId);
 }
