06cb3c6dc16cc491b21042ebb730f9dc3fb73772
cakefeelsgood <kimminji15441544@gmail.com>
2026-06-03 15:31:04 +0900

    fix: Batch 종료 처리 중 Scheduling 설정 유지 (#325)

--- src/main/java/com/moment/momentbackend/MomentBackendApplication.java
+++ src/main/java/com/moment/momentbackend/MomentBackendApplication.java
@@ -3,10 +3,12 @@ package com.moment.momentbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableScheduling
 public class MomentBackendApplication {
 
     public static void main(String[] args) {
