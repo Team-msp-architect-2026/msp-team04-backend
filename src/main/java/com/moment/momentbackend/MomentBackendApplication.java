92c42bfdaea6eb0c8c8ea5def8d59adb439f8ad7
cakefeelsgood <kimminji15441544@gmail.com>
2026-06-03 15:19:06 +0900

    fix: Batch profile 실행 후 애플리케이션 종료 처리 (#325)

--- src/main/java/com/moment/momentbackend/MomentBackendApplication.java
+++ src/main/java/com/moment/momentbackend/MomentBackendApplication.java
@@ -2,14 +2,22 @@ package com.moment.momentbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-@EnableScheduling
 public class MomentBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MomentBackendApplication.class, args);
-    }
+        ConfigurableApplicationContext context = SpringApplication.run(MomentBackendApplication.class, args);
 
+        boolean batchProfileActive = Arrays.asList(context.getEnvironment().getActiveProfiles())
+                .contains("batch");
+
+        if (batchProfileActive) {
+            int exitCode = SpringApplication.exit(context);
+            System.exit(exitCode);
+        }
+    }
 }
